cc055006af6bc2cb0bbd13205638bcde16074da3
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-16 07:21:22 +0300

    add documentContentLoaded

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 
 
+let addToy = false; 
+document.addEventListener("DOMContentLoaded", () => {
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 let divCollect = document.querySelector('#toy-collection')
-
-
 function getToys() {
   return fetch('http://localhost:3000/toys')
     .then(res => res.json())
 }
-
-function postToy(toy_data) {
+const postToy=(toy_data)=> {
   fetch('http://localhost:3000/toys', {
+    //header
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -22,7 +22,6 @@ function postToy(toy_data) {
         "name": toy_data.name.value,
         "image": toy_data.image.value,
         "likes": 0
-
       })
     })
     .then(res => res.json())
@@ -31,17 +30,15 @@ function postToy(toy_data) {
       divCollect.append(new_toy)
     })
 }
-
-function likes(e) {
+//patch method
+const likes=(e)=> {
   e.preventDefault()
   let more = parseInt(e.target.previousElementSibling.innerText) + 1
-
   fetch(`http://localhost:3000/toys/${e.target.id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json"
-
       },
       body: JSON.stringify({
         "likes": more
@@ -52,18 +49,14 @@ function likes(e) {
       e.target.previousElementSibling.innerText = `${more} likes`;
     }))
 }
-
-function renderToys(toy) {
+const renderToys=(toy)=> {
   let h2 = document.createElement('h2')
   h2.innerText = toy.name
-
   let img = document.createElement('img')
   img.setAttribute('src', toy.image)
   img.setAttribute('class', 'toy-avatar')
-
   let p = document.createElement('p')
   p.innerText = `${toy.likes} likes`
-
   let btn = document.createElement('button')
   btn.setAttribute('class', 'like-btn')
   btn.setAttribute('id', toy.id)
@@ -72,15 +65,11 @@ function renderToys(toy) {
     console.log(e.target.dataset);
     likes(e)
   })
-
   let divCard = document.createElement('div')
   divCard.setAttribute('class', 'card')
   divCard.append(h2, img, p, btn)
   divCollect.append(divCard)
 }
-
-
-
 addBtn.addEventListener('click', () => {
   
   addToy = !addToy
@@ -95,11 +84,10 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-
-
 getToys().then(toys => {
   toys.forEach(toy => {
-   
     renderToys(toy)
   })
 })
+
+});
